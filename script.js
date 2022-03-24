18c987b7f5861341fb0e6278c2368f45938450c9
Nursultan02 <nur.bek.aru.bag@gmail.com>
2022-03-24 21:41:48 +0600

    add: delete Item event

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 const todoInput = document.querySelector('.todo-list-input');
 const todoItems = document.getElementById('todo-list-items');
 
-todoInput.addEventListener('keydown', (e) => {
+let counter = 0;
+
+todoInput.addEventListener('keydown', (e) => addItem(e));
+
+const deleteItem = (itemToDelete) => {
+  itemToDelete.parentNode.removeChild(itemToDelete);
+};
+
+const addItem = (e) => {
   if (e.key === 'Enter') {
-    let val = e.target.value;
-    let li = document.createElement('li');
-    let p = document.createElement('p');
-    let button = document.createElement('button');
-    button.innerText = 'delete';
-    p.innerText = val;
-    li.appendChild(p);
-    li.appendChild(button);
-    todoItems.appendChild(li);
-    todoInput.value = '';
+    if (todoInput.value.trim() !== '') {
+      let val = e.target.value;
+
+      let li = document.createElement('li');
+      let p = document.createElement('p');
+
+      let button = document.createElement('button');
+      button.innerText = 'delete';
+
+      p.innerText = val;
+      li.appendChild(p);
+      li.appendChild(button);
+      todoItems.appendChild(li);
+
+      todoInput.value = '';
+
+      button.id = counter;
+
+      const deleteBtn = document.getElementById(counter);
+
+      deleteBtn.addEventListener('click', (e) =>
+        deleteItem(e.target.parentNode)
+      );
+      counter++;
+    } else {
+      alert('Nothing to add!');
+    }
   }
-});
+};
