b2eae47dfdf9ceebf86cbe56d601af503b2ec9b0
Nursultan02 <nur.bek.aru.bag@gmail.com>
2022-03-24 21:50:40 +0600

    add: plus button event to add element

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 const todoInput = document.querySelector('.todo-list-input');
 const todoItems = document.getElementById('todo-list-items');
+const todoListButton = document.querySelector('.todo-list-button');
 
 let counter = 0;
 
-todoInput.addEventListener('keydown', (e) => addItem(e));
+// adding element with ENTER
+todoInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    addItem();
+  }
+});
+
+// adding element with plus button
+todoListButton.addEventListener('click', () => addItem());
 
 const deleteItem = (itemToDelete) => {
   itemToDelete.parentNode.removeChild(itemToDelete);
 };
 
-const addItem = (e) => {
-  if (e.key === 'Enter') {
-    if (todoInput.value.trim() !== '') {
-      let val = e.target.value;
+const addItem = () => {
+  if (todoInput.value.trim() !== '') {
+    let val = todoInput.value;
 
-      let li = document.createElement('li');
-      let p = document.createElement('p');
+    let li = document.createElement('li');
+    let p = document.createElement('p');
 
-      let button = document.createElement('button');
-      button.innerText = 'delete';
+    let button = document.createElement('button');
+    button.innerText = 'delete';
 
-      p.innerText = val;
-      li.appendChild(p);
-      li.appendChild(button);
-      todoItems.appendChild(li);
+    p.innerText = val;
+    li.appendChild(p);
+    li.appendChild(button);
+    todoItems.appendChild(li);
 
-      todoInput.value = '';
+    todoInput.value = '';
 
-      button.id = counter;
+    button.id = counter;
 
-      const deleteBtn = document.getElementById(counter);
+    const deleteBtn = document.getElementById(counter);
 
-      deleteBtn.addEventListener('click', (e) =>
-        deleteItem(e.target.parentNode)
-      );
-      counter++;
-    } else {
-      alert('Nothing to add!');
-    }
+    deleteBtn.addEventListener('click', (e) => deleteItem(e.target.parentNode));
+    counter++;
+  } else {
+    alert('Nothing to add!');
   }
 };
