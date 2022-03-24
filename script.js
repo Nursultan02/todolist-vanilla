b65e967c3df1d14aff1a6e95e1abc888af6d40c8
Nursultan02 <nur.bek.aru.bag@gmail.com>
2022-03-25 00:45:38 +0600

    add: complted button and eventHandler

--- script.js
+++ script.js
@@ -18,27 +18,48 @@ const deleteItem = (itemToDelete) => {
   itemToDelete.parentNode.removeChild(itemToDelete);
 };
 
+const addStyleToCompletedItem = (e) => {
+  item = e.target.parentNode.parentNode;
+  item.classList.add('completed');
+  item.firstChild.style.color = 'green';
+  e.target.parentNode.removeChild(e.target);
+};
+
 const addItem = () => {
   if (todoInput.value.trim() !== '') {
     let val = todoInput.value;
 
     let li = document.createElement('li');
     let p = document.createElement('p');
-    let button = document.createElement('button');
+    let deleteButton = document.createElement('button');
+    let completedButton = document.createElement('button');
+    let buttonWrapper = document.createElement('div');
 
-    button.innerText = 'Delete';
+    deleteButton.innerText = 'Delete';
     p.innerText = val;
+    completedButton.innerText = 'Completed';
+
+    buttonWrapper.appendChild(completedButton);
+    buttonWrapper.appendChild(deleteButton);
 
     li.appendChild(p);
-    li.appendChild(button);
+    li.appendChild(buttonWrapper);
+
     todoItems.appendChild(li);
 
-    button.id = counter;
-    button.className = 'deleteBtn';
+    deleteButton.id = counter;
+    deleteButton.className = 'btn deleteBtn';
+    completedButton.className = 'btn completed-btn';
 
     const deleteBtn = document.getElementById(counter);
 
-    deleteBtn.addEventListener('click', (e) => deleteItem(e.target.parentNode));
+    deleteBtn.addEventListener('click', (e) =>
+      deleteItem(e.target.parentNode.parentNode)
+    );
+
+    completedButton.addEventListener('click', (e) =>
+      addStyleToCompletedItem(e)
+    );
 
     todoInput.value = '';
     counter++;
